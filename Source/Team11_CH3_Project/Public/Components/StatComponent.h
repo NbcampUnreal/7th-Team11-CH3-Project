0d448926ee4382e6b7e66ee768151d41243f2af0
brian521 <59503208+brian521@users.noreply.github.com>
2026-02-09 20:28:16 +0900

    Add StatComponent

--- Source/Team11_CH3_Project/Public/Components/StatComponent.h
+++ Source/Team11_CH3_Project/Public/Components/StatComponent.h
@@ -1,28 +1,78 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "StatComponent.generated.h"
 
+UENUM(BlueprintType)
+enum class EStat : uint8
+{
+	HP,
+	MaxHP,
+	DEF,
+	MoveSpeed,
+	AttackDamage,
+	CastSpeed,
+	ProjectileSpeed,
+	CriticalChance,
+	CriticalDamage,
+};
+
+USTRUCT()
+struct TEAM11_CH3_PROJECT_API FStatData
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	float HP;
+	UPROPERTY()
+	float MaxHP;
+	UPROPERTY()
+	float DEF;
+	UPROPERTY()
+	float MoveSpeed;
+	UPROPERTY()
+	float AttackDamage;
+	UPROPERTY()
+	float CastSpeed;
+	UPROPERTY()
+	float ProjectileSpeed;
+	UPROPERTY()
+	float CriticalChance;
+	UPROPERTY()
+	float CriticalDamage;
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TEAM11_CH3_PROJECT_API UStatComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	UStatComponent();
 
-protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	// 기본 스탯 설정
+	void SetBaseStat(const FStatData& StatData);
+	// 기본 특정 스탯 설정
+	void SetBaseStat(EStat TargetStat, float Amount);
+	// 현재 특정 스탯 설정
+	void SetCurrentStat(EStat TargetStat, float Amount);
 
+	// 기본 스탯 가져오기
+	float GetBaseStat(EStat TargetStat) const;
+	// 현재 스탯 가져오기
+	float GetCurrentStat(EStat TargetStat) const;
+
+	// 받는 데미지 계산
+	bool TakeDamage(float DamageAmount);
 		
+public:
+	UPROPERTY()
+	FStatData CurrentStat;
+
+private:
+	UPROPERTY()
+	FStatData BaseStat;
 };
