560e6ac7ed024ff31a28b4dba3dfe02ec1d989a7
jcmeve <jcmeve@naver.com>
2026-02-10 12:38:40 +0900

    Add GM
    Add CM

---
+++ Source/Team11_CH3_Project/Public/GM_Default.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GM_Default.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM11_CH3_PROJECT_API AGM_Default : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
