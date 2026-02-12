93e420bd991eab608340f82b8d91312c54bb0701
Yoongisang <sawsd@naver.com>
2026-02-12 20:27:48 +0900

    Add item system classes and ItemManager
    아이템 시스템(아이템 매니저, 장비, 포션 등)

---
+++ Source/Team11_CH3_Project/Public/Components/Items/PotionItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Items/ItemBase.h"
+#include "PotionItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM11_CH3_PROJECT_API UPotionItem : public UItemBase
+{
+	GENERATED_BODY()
+	
+};
