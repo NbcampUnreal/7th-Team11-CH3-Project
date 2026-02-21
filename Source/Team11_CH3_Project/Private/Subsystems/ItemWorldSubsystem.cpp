17fd21854dfff6e86922258e9ffa1ac4625a830c
Yoongisang <sawsd@naver.com>
2026-02-21 23:23:32 +0900

    Add ItemWorldSubsystem for item data
    아이템 데이터 테이블을 담당할 WorldSubsystem 생성 및 구현

---
+++ Source/Team11_CH3_Project/Private/Subsystems/ItemWorldSubsystem.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystems/ItemWorldSubsystem.h"
+
+void UItemWorldSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	// 데이터 테이블 로드
+	WeaponTable   = LoadObject<UDataTable>(nullptr, TEXT("/Game/Data/DataTable/DT_Armor"));
+	ArmorTable    = LoadObject<UDataTable>(nullptr, TEXT("/Game/Data/DataTable/DT_Armor"));
+	PotionTable   = LoadObject<UDataTable>(nullptr, TEXT("/Game/Data/DataTable/DT_Potion"));
+	SkillGemTable = LoadObject<UDataTable>(nullptr, TEXT("/Game/Data/DataTable/DT_SkillGem"));
+}
+
+FWeaponItemData* UItemWorldSubsystem::FindWeapon(FName RowName)
+{
+	return WeaponTable ? WeaponTable->FindRow<FWeaponItemData>(RowName, TEXT("FindWeapon")) : nullptr;
+}
+
+FArmorItemData* UItemWorldSubsystem::FindArmor(FName RowName)
+{
+	return ArmorTable ? ArmorTable->FindRow<FArmorItemData>(RowName, TEXT("FindArmor")) : nullptr;
+}
+
+FPotionItemData* UItemWorldSubsystem::FindPotion(FName RowName)
+{
+	return PotionTable ? PotionTable->FindRow<FPotionItemData>(RowName, TEXT("FindPotion")) : nullptr;
+}
+
+FSkillGemItemData* UItemWorldSubsystem::FindSkillGem(FName RowName)
+{
+	return SkillGemTable ? SkillGemTable->FindRow<FSkillGemItemData>(RowName, TEXT("FindSkillGem")) : nullptr;
+}
---
+++ Source/Team11_CH3_Project/Public/Subsystems/ItemWorldSubsystem.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Components/Items/PotionItemData.h"
+#include "Components/Items/SkillGemItemData.h"
+#include "Components/Items/Equipments/WeaponItemData.h"
+#include "Components/Items/Equipments/ArmorItemData.h"
+#include "ItemWorldSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM11_CH3_PROJECT_API UItemWorldSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	FWeaponItemData* FindWeapon(FName RowName);
+	FArmorItemData* FindArmor(FName RowName);
+	FPotionItemData* FindPotion(FName RowName);
+	FSkillGemItemData* FindSkillGem(FName RowName);
+
+private:
+	UPROPERTY()
+	TObjectPtr<UDataTable> WeaponTable;
+	UPROPERTY()
+	TObjectPtr<UDataTable> ArmorTable;
+	UPROPERTY()
+	TObjectPtr<UDataTable> PotionTable;
+	UPROPERTY()
+	TObjectPtr<UDataTable> SkillGemTable;
+};
